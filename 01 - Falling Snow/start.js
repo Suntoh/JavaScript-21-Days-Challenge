8a335efb5540c99dbd7e3ad40d73515523412ccc
Suntoh <weerawit.j@gmail.com>
2023-10-10 15:26:46 +0700

    1

--- 01 - Falling Snow/start.js
+++ 01 - Falling Snow/start.js
@@ -1,4 +1,67 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function setup() {
+    const canvas = document.getElementById('falling-snow-canvas');
+    canvas.width =window.innerWidth;
+    canvas.height =window.innerHeight;
+    return {
+      canvas,
+      canvasContext: canvas.getContext('2d'),
+      numberOfSnowBalls: 450
+    }
+  }
+  function random(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+
+  }
+  function snowBalls(canvas, numberOfSnowBalls) {
+    return [...Array(numberOfSnowBalls)].map(() => {
+      return{
+        x:random(0, canvas.width),
+        y: random(0, canvas.height),
+        brightness:   random(0.2,1),
+        radius: random(2,5),
+        speedX : random(-5,5),
+        speedY : random(1,4)
+      }
+    });
+  }
+
+  function drawSnowBall(canvasContext,snowBalls){
+    canvasContext.beginPath ();
+    canvasContext.arc(snowBalls.x,snowBalls.y,snowBalls.radius,0,Math.PI * 2);
+    canvasContext.fillStyle = `rgba(255,255,255,${snowBalls.brightness})`;
+    canvasContext.fill();
+  }
+
+  function moveSnowBall(canvas,snowBalls){
+    snowBalls.x += snowBalls.speedX;
+    snowBalls.y += snowBalls.speedY;
+
+    if(snowBalls.x > canvas.width){
+      snowBalls.x = 0;
+    }
+    else if(snowBalls.x < 0){
+      snowBalls.x = canvas.width;
+    }
+
+    if (snowBalls.y > canvas.height){
+      snowBalls.y = 0;
+    }
+}
+
+  function run() {
+    const { canvas,canvasContext,numberOfSnowBalls } = setup();
+    const snowBallss = snowBalls( canvas, numberOfSnowBalls);
+  
+    setInterval( () =>{
+      canvasContext.clearRect(0,0,canvas.width,canvas.height);
+      snowBallss.forEach((snowBall) => drawSnowBall(canvasContext,snowBall));
+      snowBallss.forEach((snowBall) => moveSnowBall(canvas,snowBall));
+    },50)
+    
+  }
+
+  run();
 
 })();
--- 02 - Countdown Timer/start.js
+++ 02 - Countdown Timer/start.js
@@ -1,4 +1,44 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const second = 1000;
+  const minute = second *60;
+  const hour = minute *60;
+  const day = hour * 24;
+
+
+  function setElementInnerText(id,text){
+    const element = document.getElementById(id);
+    element.innerText = text
+  }
+
+  function countDown(){
+    const now = new Date().getTime();
+    const newYear = new Date('December 31, 2023 23:59:59').getTime();
+    const unixTimeleft = newYear - now;
+
+    // const daysElement = document.getElementById('days');
+    // daysElement.innerText = Math.floor(unixTimeleft / day)
+    // const hoursElement = document.getElementById('hours');
+    // hoursElement.innerText = Math.floor(unixTimeleft % day/ hour);
+    // const minutesElement = document.getElementById('minutes');
+    // minutesElement = document.getElementById(unixTimeleft %hour/ minute);
+    // const secondsElement = document.getElementById('seconds');
+    // secondsElement = document.getElementById(unixTimeleft %minute /seconds);
+    //too long
+
+    setElementInnerText('days',Math.floor(unixTimeleft / day));
+    setElementInnerText('hours',Math.floor(unixTimeleft % day/ hour));
+    setElementInnerText('minutes',Math.floor(unixTimeleft % hour/ minute));
+    setElementInnerText('seconds',Math.floor(unixTimeleft % minute/second));
+  
+  }
+
+
+  
+  function run(){
+    setInterval(countDown, second)
+  }
+
+  run()
 })();
