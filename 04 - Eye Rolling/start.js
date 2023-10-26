6629bd9ec423e7dea4c6526a2ccc543f599953e6
Suntoh <weerawit.j@gmail.com>
2023-10-26 17:38:35 +0700

    3

--- 04 - Eye Rolling/start.js
+++ 04 - Eye Rolling/start.js
@@ -1,4 +1,23 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function run(){
+    const bodyElement = document.querySelector("body");
+    const eyeElement = document.querySelectorAll(".eye");
+    
+    function onMouseMove({pageX,pageY}){
+      eyeElement.forEach((eyeElement) => {
+        const{left,top} //เอาตำแหน่งจากซ้ายบน(ตำแหน่งตาขาว เป็นสี่เหลี่ยม)
+        = eyeElement.getBoundingClientRect();
+        const eyeCenterX = left + eyeElement.offsetWidth / 2;
+        const eyeCenterY = top + eyeElement.offsetHeight / 2;
+        const radian = Math.atan2(pageX - eyeCenterX, pageY - eyeCenterY);
+        const angle = radian * 180/ Math.PI * -1 ;
 
+        eyeElement.style.transform = `rotate(${angle}deg)`
+      });
+    }
+
+    bodyElement.addEventListener('mousemove', onMouseMove);
+  }
+  run()
 })();
