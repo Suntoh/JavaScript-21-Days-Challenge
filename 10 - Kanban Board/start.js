8fce7e2def919b82dc51e615bd7bb38581b8cdc4
Suntoh <weerawit.j@gmail.com>
2023-11-07 14:58:50 +0700

    6

--- 10 - Kanban Board/start.js
+++ 10 - Kanban Board/start.js
@@ -1,4 +1,37 @@
 (() => {
   // เริ่มเขียนโค้ด
+  let draggingElem;
 
+
+  function run(){
+    const taskElems = Array.from(document.querySelectorAll(".task"));
+    const dropZoneElems = Array.from(document.querySelectorAll(".drop-zone"));
+
+    taskElems.forEach((taskElem) => {
+      taskElem.addEventListener('dragstart',ondragstart);
+    })
+    dropZoneElems.forEach((dropZoneElem) =>{
+      dropZoneElem.addEventListener('drop',ondrop);
+      dropZoneElem.addEventListener('dragover',ondragover);
+      dropZoneElem.addEventListener('drageter',ondragenter);
+
+    })
+
+  }
+
+  function ondrop(event){
+    this.append(draggingElem);
+    draggingElem = null;
+  }
+  function ondragstart(){
+    draggingElem =this;
+  }
+  function ondragenter(event){
+    event.preventDefault();
+  }
+  function ondragover(event){
+    event.preventDefault();
+  }
+
+  run()
 })();
