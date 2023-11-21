de472242a9d365070ac0eba6058dbd0d26d960e9
Suntoh <weerawit.j@gmail.com>
2023-11-21 16:53:38 +0700

    8

--- 14 -  Carousel/start.js
+++ 14 -  Carousel/start.js
@@ -1,4 +1,27 @@
 (() => {
   // เริ่มเขียนโค้ด
+  let index = 0;
+
+
+  function displayImage(imageElems,newIndex){
+    if (newIndex === -1){
+      newIndex = imageElems.length-1;
+    }
+    else if(newIndex === imageElems.length){
+      newIndex = 0;
+    }
+    const newImage = imageElems[newIndex];
+    newImage.scrollIntoView({ behavior: 'smooth' });
+    index  = newIndex;
+  }
+  function run(){
+    const imageElems = document.querySelectorAll('img');
+    const previousBtnElem = document.querySelector('.previous');
+    const nextBtnElem = document.querySelector('.next');
+
+    previousBtnElem.addEventListener('click', () => displayImage(imageElems,index-1))
+    nextBtnElem.addEventListener('click',() => displayImage(imageElems,index +1))
+  }
+  run();
 
 })();
--- 15 - Music Player/start.js
+++ 15 - Music Player/start.js
@@ -1,4 +1,62 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const audioElem = document.querySelector('audio');
+  const playBtnElem = document.querySelector('.play');
+  const progressionBarElem = document.querySelector('.progress-bar');
+  const startTimeElem = document.querySelector('.start-time');
+  const endTimeElem = document.querySelector('.end-time');
+
+  function onclick(){
+    if(audioElem.paused){
+      audioElem.play();
+      playBtnElem.className = 'pause';
+    }else{
+      audioElem.pause();
+      playBtnElem.className = 'play';
+    }
+
+  }
+
+  function getDuration(time){
+    const min = Math.floor(time/60 % 60);
+    const sec = Math.floor(time % 60).toString().padStart(2,'0');
+    
+    return `${min}:${sec}`;
+  }
+
+
+  function ontimeupdate(){
+    startTimeElem.innerHTML = getDuration(audioElem.currentTime);
+    progressionBarElem.value = audioElem.currentTime;
+  }
+
+  function onloadeddata(){
+    endTimeElem.innerHTML = getDuration(audioElem.duration);
+    progressionBarElem.max  =audioElem.duration;
+  }
+  function onRefresh(audioElem){
+    endTimeElem.innerHTML = getDuration(audioElem.duration);
+    progressionBarElem.max  =audioElem.duration;
+  }
+  function onInput(){
+    audioElem.currentTime = progressionBarElem.value;
+  }
+  function onended(){
+    playBtnElem.className = 'play';
+    audioElem.currentTime = 0;
+  }
+  function run(){
+    playBtnElem.addEventListener('click', onclick);
+    
+    audioElem.addEventListener('timeupdate',ontimeupdate);
+    audioElem.addEventListener('loadeddata',onloadeddata);
+    if(endTimeElem.innerHTML === '0:00'){
+      onRefresh(audioElem);
+    }
+    audioElem.addEventListener('ended',onended);
+    progressionBarElem.addEventListener('input', onInput);
+  }
+
+  run()
 })();
