e32a8b2714950329f100e04cfc1374aa564e1712
suntoh <weerawit.j@gmail.com>
2023-11-22 19:17:48 +0700

    9

--- 16 - Text to Speech/start.js
+++ 16 - Text to Speech/start.js
@@ -1,4 +1,29 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  const message = new SpeechSynthesisUtterance();
+
+  function onVoiceChanged(){
+    const voices = speechSynthesis.getVoices();
+    const thVoice = voices.find(voice => voice.lang === 'th-T');
+    message.voice  = thVoice;
+  }
+
+  function onclick(event){
+    message.text = event.target.getAttribute('alt')
+    speechSynthesis.speak(message);
+
+  }
+
+
+  function run(){
+    speechSynthesis.addEventListener('voiceschanged',onVoiceChanged)
+
+    const imageElems = Array.from(document.querySelectorAll('img'));
+    imageElems.forEach(imageElem => imageElem.addEventListener('click',onclick));
+  }
+
+  run();
+
+
 })();
--- 17 - Speech to Text/start.js
+++ 17 - Speech to Text/start.js
@@ -1,3 +1,51 @@
 (() => {
   // เริ่มเขียนโค้ด
+
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new window.SpeechRecognition();
+  const btnElem = document.querySelector('.control');
+
+
+  function onclick(){
+    const isPausing = btnElem.classList.contains('record');
+
+    if(isPausing){
+      recognition.start();
+      btnElem.classList.remove('record');
+      btnElem.classList.add('pause');
+    }else{
+      recognition.stop();
+      btnElem.classList.remove('pause');
+      btnElem.classList.add('record');
+    }
+  }
+
+  function onResult(event){
+    const textElem = document.querySelector('.text');
+    console.log(event.results);
+    const {transcript} = event.results[0][0];
+    textElem.innerText += transcript;
+
+
+  }
+  function onended(){
+    const isRecording = btnElem.classList.contains('pause');
+    if(isRecording){
+      recognition.start();
+    }
+  }
+
+  function run(){
+    recognition.lang = 'th-TH';
+
+
+    recognition.addEventListener('result',onResult)
+    recognition.addEventListener('end',onended);
+    btnElem.addEventListener('click',onclick);
+
+  }
+  run()
+
+
 })();
